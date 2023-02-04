1376a654a0a720a105f28c6a681a83ab9a572c6a
DarthVanger <DarthVanger@users.noreply.github.com>
2023-02-04 13:08:36 +0000

    Remove preview for PR 36 🛬

--- pr-preview/pr-36/touch.js
+++
@@ -1,8 +0,0 @@
-let checkDevice = navigator.userAgent;
-let touch = document.querySelector('#keyboard-controls-help');
-
-if (navigator.userAgent.match('iPhone') || navigator.userAgent.match('Android') || 
-    navigator.userAgent.match('iPad') || navigator.userAgent.match('RIM')) {
-    document.body.classList.add('_touch');
-    touch.style.display = 'none';
-} 
