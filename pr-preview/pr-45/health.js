332c510a323887c745557fbe10d0b9f03a0e1d06
Alianshark <Alianshark@users.noreply.github.com>
2023-02-11 13:13:41 +0000

    Deploy preview for PR 45 🛫

---
+++ pr-preview/pr-45/health.js
@@ -0,0 +1,25 @@
+export let enemyHealth50 = {
+    velocity: -2,
+    element: document.querySelector('#healthBar50')
+  };
+export let enemyHealth100 = {
+    velocity: -2,
+    element: document.querySelector('#healthBar100')
+  };
+export let enemyHealth200 = {
+    velocity: -2,
+    element: document.querySelector('#healthBar200')
+  };
+export let enemyHealth50text = {
+    velocity: -2,
+    element: document.querySelector('#healthBar50text')
+  };
+export let enemyHealth100text = {
+    velocity: -2,
+    element: document.querySelector('#healthBar100text')
+  };
+export let enemyHealth200text = {
+    velocity: -2,
+    element: document.querySelector('#healthBar200text')
+  };
+  
