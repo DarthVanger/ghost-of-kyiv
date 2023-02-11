874168582cfc3fab8a46b44c565ba05fc9543c73
Alianshark <Alianshark@users.noreply.github.com>
2023-02-11 16:59:42 +0000

    Deploy preview for PR 50 🛫

---
+++ pr-preview/pr-50/enemy.js
@@ -0,0 +1,26 @@
+export let enemy1 = {
+    x: 800,
+    y: 100,
+    width: 360,
+    height: 200,
+    velocity: -2,
+    element: document.querySelector("#enemy1"),
+  };
+  
+export let enemy2 = {
+    x: 1400,
+    y: 350,
+    width: 350,
+    height: 240,
+    velocity: -2,
+    element: document.querySelector("#enemy21"),
+  };
+  
+export let enemy3 = {
+    x: 1900,
+    y: 250,
+    width: 350,
+    height: 350,
+    velocity: -2,
+    element: document.querySelector("#enemy3"),
+  };
