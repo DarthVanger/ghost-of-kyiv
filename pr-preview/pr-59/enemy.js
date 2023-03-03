220249d1c730b9bc61399f9b0236fca4dea12bd1
DarthVanger <DarthVanger@users.noreply.github.com>
2023-03-03 19:23:08 +0000

    Deploy preview for PR 59 🛫

---
+++ pr-preview/pr-59/enemy.js
@@ -0,0 +1,26 @@
+export let enemy1 = {
+    x: 800,
+    y: 100,
+    width: 250,
+    height: 80,
+    velocity: -2,
+    element: document.querySelector("#enemy1"),
+  };
+  
+export let enemy2 = {
+    x: 1400,
+    y: 350,
+    width: 270,
+    height: 100,
+    velocity: -2,
+    element: document.querySelector("#enemy21"),
+  };
+  
+export let enemy3 = {
+    x: 1900,
+    y: 250,
+    width: 330,
+    height: 200,
+    velocity: -2,
+    element: document.querySelector("#enemy3"),
+  };
