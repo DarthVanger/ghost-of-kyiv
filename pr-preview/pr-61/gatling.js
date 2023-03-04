5549daf0d40d081dcc7ce1c778bd3b2dd78f925a
19thanks96 <19thanks96@users.noreply.github.com>
2023-03-04 18:30:10 +0000

    Deploy preview for PR 61 🛫

---
+++ pr-preview/pr-61/gatling.js
@@ -0,0 +1,53 @@
+import {enemyHealth50, enemyHealth100, enemyHealth200, enemyHealth50text, enemyHealth100text, enemyHealth200text} from "./health.js";
+import {airfighter} from "./airfighter.js";
+import {enemy1, enemy2, enemy3} from './enemy.js';
+import { soundExplosion, soundGatling} from "./music.js";
+
+export const enemyDies = 9999;
+
+export let gatling = {
+  x: 0,
+  y: 0,
+  velocity: -2,
+  ammo: 1500,
+  dmg: 10,
+  element: document.querySelector("#gatling"),
+};
+
+export function fireGatlingEnemyOne (event) {  
+    if (airfighter.x + airfighter.width < event.pageX ) { 
+      healthBar50.value -= gatling.dmg;
+      gatling.ammo -= 10;
+      soundGatling.play();
+      if (healthBar50.value === 0) {
+        enemy1.x -= enemyDies;
+        soundExplosion.play();
+      }
+    }
+  }
+  
+  export function fireGatlingEnemyTwo (event) { 
+    if (airfighter.x + airfighter.width < event.pageX ) {
+      enemyHealth100.element.value -= gatling.dmg;
+      gatling.ammo -= 10;
+      soundGatling.play();
+      if (enemyHealth100.element.value === 0) {
+        console.log('enemy2');
+        console.log(enemy2);
+        enemy2.x -= enemyDies;
+        soundExplosion.play();
+      }
+    }
+   }
+  
+   export function fireGatlingEnemyThree (event) {
+    if (airfighter.x + airfighter.width < event.pageX ) {
+      healthBar200.value -= gatling.dmg;
+      gatling.ammo -= 10;
+      soundGatling.play();
+      if (healthBar200.value === 0) {
+        enemy3.x -= enemyDies;
+        soundExplosion.play();
+      }
+    }
+  }
