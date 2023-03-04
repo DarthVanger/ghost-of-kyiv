8d4b527ec41eb73276e226f1fdfa8dd21f69d5f7
hellgamer <19thanks96@gmail.com>
2023-03-04 23:36:53 +0200

    no user select, draggable always false,

--- music.js
+++ music.js
@@ -4,8 +4,8 @@ export let soundHit = new Audio();
 export let soundExplosion = new Audio();
 export let soundLaught = new Audio();
 
-soundGatling.src = 'avtomat.mp3';
-soundShot.src = 'vyistrel.mp3';
-soundHit.src = 'smallBoom.mp3';
-soundExplosion.src = 'vzryiv-sredney.mp3';
-soundLaught.src = 'zvuk-so-smehom-nelsona-iz-simpsonov-ha-ha-6710-1__=3.mp3';
+soundGatling.src = 'music/avtomat.mp3';
+soundShot.src = 'music/vyistrel.mp3';
+soundHit.src = 'music/smallBoom.mp3';
+soundExplosion.src = 'music/vzryiv-sredney.mp3';
+soundLaught.src = 'music/zvuk-so-smehom-nelsona-iz-simpsonov-ha-ha-6710-1__=3.mp3';
