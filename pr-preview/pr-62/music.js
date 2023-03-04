74d5000f94c4aaa7c1be98a35420df057c4a1837
19thanks96 <19thanks96@users.noreply.github.com>
2023-03-04 21:43:45 +0000

    Deploy preview for PR 62 🛫

---
+++ pr-preview/pr-62/music.js
@@ -0,0 +1,11 @@
+export let soundGatling = new Audio();
+export let soundShot = new Audio();
+export let soundHit = new Audio();
+export let soundExplosion = new Audio();
+export let soundLaught = new Audio();
+
+soundGatling.src = 'music/avtomat.mp3';
+soundShot.src = 'music/vyistrel.mp3';
+soundHit.src = 'music/smallBoom.mp3';
+soundExplosion.src = 'music/vzryiv-sredney.mp3';
+soundLaught.src = 'music/zvuk-so-smehom-nelsona-iz-simpsonov-ha-ha-6710-1__=3.mp3';
