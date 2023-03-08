9c4755b8211ab0ee9fea230ff9d511c70482f4d8
19thanks96 <19thanks96@users.noreply.github.com>
2023-03-08 15:08:09 +0000

    Deploy preview for PR 63 🛫

---
+++ pr-preview/pr-63/music.js
@@ -0,0 +1,15 @@
+export let soundGatling = new Audio();
+export let soundRocketShot = new Audio();
+export let soundRocketHit = new Audio();
+export let soundEnemyDieExplosion = new Audio();
+export let soundGameOver = new Audio();
+export let soundMainTheme = new Audio();
+export let soundLevelComplete = new Audio();
+
+soundGatling.src = 'music/avtomat.mp3';
+soundRocketShot.src = 'music/vyistrel.mp3';
+soundRocketHit.src = 'music/smallBoom.mp3';
+soundEnemyDieExplosion.src = 'music/vzryiv-sredney.mp3';
+soundGameOver.src = 'music/zvuk-so-smehom-nelsona-iz-simpsonov-ha-ha-6710-1__=3.mp3';
+soundMainTheme.src = 'music/ES_Step by Step - Experia.mp3';
+soundLevelComplete.src = 'music/ES_Phoenix Rising - Edgar Hopp.mp3';
