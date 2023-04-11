c70d4c7c82529da4acbd6f2cebcd080dfc052647
19thanks96 <19thanks96@users.noreply.github.com>
2023-04-11 09:13:07 +0000

    Deploy preview for PR 80 🛫

---
+++ pr-preview/pr-80/enemy.js
@@ -0,0 +1,122 @@
+export let enemies = []
+
+function createFirstEnemyHealth(obj, i) {
+  obj.e.src = 'img/su-3.png';
+  obj.width = 250;
+  obj.height = 80;
+
+  obj.enemyHealth = {};
+  obj.enemyHealth.velocity = -2;
+  obj.enemyHealth.elem = document.createElement('meter')
+  obj.enemyHealth.elem.setAttribute('min',"0")
+  obj.enemyHealth.elem.setAttribute('low',"15")
+  obj.enemyHealth.elem.setAttribute('high',"20")
+  obj.enemyHealth.elem.setAttribute('optimum',"50")
+  obj.enemyHealth.elem.setAttribute('max',"50")
+  obj.enemyHealth.elem.setAttribute('value',"50")
+  obj.enemyHealth.elem.id = 'healthBar50-' + i;
+  obj.enemyHealth.elem.className = 'healthBar50';
+  document.body.append(obj.enemyHealth.elem);
+
+  obj.enemyHealthText =  {};
+  obj.enemyHealthText.velocity = -2;
+  obj.enemyHealthText.elem = document.createElement('div')
+  obj.enemyHealthText.elem.id = 'healthBar50text-' + i;
+  obj.enemyHealthText.elem.className = 'healthBar50text';
+  document.body.append(obj.enemyHealthText.elem);
+
+  obj.enemyHealth.element = document.querySelector('#healthBar50-' + i);
+  obj.enemyHealthText.element = document.querySelector('#healthBar50text-' + i);
+}
+
+function createSecondEnemyHealth(obj, i){
+  obj.e.src = 'img/su-27.png';
+  obj.width = 270;
+  obj.height =  100;
+
+  obj.enemyHealth = {};
+  obj.enemyHealth.velocity = -2;
+  obj.enemyHealth.elem = document.createElement('meter');
+  obj.enemyHealth.elem.setAttribute('min', 0);
+  obj.enemyHealth.elem.setAttribute('low', 25);
+  obj.enemyHealth.elem.setAttribute('high', 50);
+  obj.enemyHealth.elem.setAttribute('optimum', 100);
+  obj.enemyHealth.elem.setAttribute('max', 100);
+  obj.enemyHealth.elem.setAttribute('value', 100);
+  obj.enemyHealth.elem.id = 'healthBar100-' + i;
+  obj.enemyHealth.elem.className = 'healthBar100';
+  document.body.append(obj.enemyHealth.elem);
+
+  obj.enemyHealthText = {};
+  obj.enemyHealthText.velocity = -2;
+  obj.enemyHealthText.elem = document.createElement('div')
+  obj.enemyHealthText.elem.id = 'healthBar100text-' + i;
+  obj.enemyHealthText.elem.className = 'healthBar100text';
+  document.body.append(obj.enemyHealthText.elem);
+  
+  obj.enemyHealth.element = document.querySelector('#healthBar100-' + i);
+  obj.enemyHealthText.element = document.querySelector('#healthBar100text-' + i);
+}
+
+function createThirdEnemyHealth(obj, i) {
+  obj.e.src = 'img/z-10.png';
+    obj.width = 330;
+    obj.height = 200;
+
+    obj.enemyHealth = {};
+    obj.enemyHealth.velocity = -2;
+    obj.enemyHealth.elem = document.createElement('meter')
+    obj.enemyHealth.elem.setAttribute('min', 0)
+    obj.enemyHealth.elem.setAttribute('low', 70)
+    obj.enemyHealth.elem.setAttribute('high', 140)
+    obj.enemyHealth.elem.setAttribute('optimum', 200)
+    obj.enemyHealth.elem.setAttribute('max', 200)
+    obj.enemyHealth.elem.setAttribute('value', 200)
+    obj.enemyHealth.elem.id = 'healthBar200-' + i;
+    obj.enemyHealth.elem.className = 'healthBar200';
+    document.body.append(obj.enemyHealth.elem);
+    
+    obj.enemyHealthText = {};
+    obj.enemyHealthText.velocity = -2;
+    obj.enemyHealthText.elem = document.createElement('div')
+    obj.enemyHealthText.elem.id = 'healthBar200text-' + i;
+    obj.enemyHealthText.elem.className = 'healthBar200text';
+    document.body.append(obj.enemyHealthText.elem);
+
+    obj.enemyHealth.element = document.querySelector('#healthBar200-' + i);
+    obj.enemyHealthText.element = document.querySelector('#healthBar200text-' + i)
+}
+
+function createEnemies() {
+for(let i = 0; i < 11; i++) {  
+    let obj = {};
+    obj.e = document.createElement('img')
+    obj.e.id = 'enemy' + i;
+
+    if(i < 5){
+    createFirstEnemyHealth(obj, i)
+  } else if( i >= 5 && i <= 9) {
+    createSecondEnemyHealth(obj, i)
+  } else{
+    createThirdEnemyHealth(obj, i)
+  }
+
+    document.body.append(obj.e)
+    obj.name = 'enemy-' + i;
+    obj.isAlive = true;
+    obj.x = 0;
+
+    if(i < 1) {
+      obj.x = Math.floor(Math.random()*400)+400;
+    } else {
+      obj.x = Math.floor(Math.random()*400)+300+(enemies[i-1].x);
+    }
+
+    obj.y = Math.floor(Math.random()*(innerHeight-200));
+    obj.velocity = -2;
+    obj.element = document.querySelector('#enemy' + i);
+    enemies.push(obj)
+  }
+}
+
+createEnemies()
---
+++ pr-preview/pr-80/health.js
@@ -0,0 +1,19 @@
+export let enemyHealth50 = {
+    velocity: -2,
+  };
+export let enemyHealth100 = {
+    velocity: -2,
+  };
+export let enemyHealth200 = {
+    velocity: -2,
+  };
+export let enemyHealth50text = {
+    velocity: -2,
+  };
+export let enemyHealth100text = {
+    velocity: -2,
+  };
+export let enemyHealth200text = {
+    velocity: -2,
+  };
+  
