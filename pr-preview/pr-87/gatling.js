59e6cb6f02a56828190b4ac0d14eb136a00d68f9
19thanks96 <19thanks96@users.noreply.github.com>
2023-04-12 13:50:57 +0000

    Deploy preview for PR 87 🛫

---
+++ pr-preview/pr-87/gatling.js
@@ -0,0 +1,23 @@
+import {airfighter} from "./airfighter.js";
+import {soundEnemyDieExplosion, soundGatling} from "./music.js";
+
+export const enemyDies = 9999;
+
+export let gatling = {
+  velocity: -2,
+  ammo: 1500,
+  dmg: 10,
+  element: document.querySelector("#gatling"),
+};
+
+export function fireGatlingEnemy (event, enemy) {  
+    if (airfighter.x + airfighter.width < event.pageX ) { 
+      enemy.enemyHealth.element.value -= gatling.dmg;
+      gatling.ammo -= 10;
+      soundGatling.play();
+      if (enemy.enemyHealth.element.value === 0) {
+        enemy.x -= enemyDies;
+        soundEnemyDieExplosion.play();
+      }
+    }
+  }
---
+++ pr-preview/pr-87/touch.js
@@ -0,0 +1,14 @@
+let checkDevice = navigator.userAgent;
+const viewport = document.querySelector('#viewport'); 
+let touch = document.querySelector('#keyboard-controls-help');
+
+if (navigator.userAgent.match('iPhone') || navigator.userAgent.match('Android') || 
+    navigator.userAgent.match('iPad') || navigator.userAgent.match('RIM')) {
+    document.body.classList.add('_touch');
+    touch.style.display = 'none';
+    if (window.orientation === 0) {
+    viewport.setAttribute('content', 'width=900, initial-scale=0.3, max-scale=0.3, user-scalable=no');
+    }
+} 
+
+document.querySelectorAll('*').forEach((e) =>{e.setAttribute('draggable', false)}); 
