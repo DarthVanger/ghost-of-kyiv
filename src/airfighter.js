cd6f86cef5b4bc850a591d67875d98fcbb872f5f
StanislavMck2120 <stanislavmat@rozdoum.com>
2023-06-03 23:05:22 +0300

    airfigther refactoring

--- src/airfighter.js
+++ src/airfighter.js
@@ -1,7 +1,12 @@
-import { rocket } from "./rocket.js";
-import { soundRocketHit, soundEnemyDieExplosion } from "./music.js";
+import { rocket } from './rocket.js'
+import {
+  soundRocketHit,
+  soundEnemyDieExplosion,
+  soundMainTheme,
+  soundGameOver,
+} from './music.js'
 
-const acceleration = 1;
+const acceleration = 1
 
 class Player {
   x = 0
@@ -21,128 +26,123 @@ class Player {
     y: 0,
     element: document.querySelector('#playerHealthText'),
   }
-  rocketDefaultX = 80
-  rocketDefaultY = 70
   rocketMaxDistance = 1500
   isShipMovingUp = false
   isShipMovingLeft = false
   isShipMovingRight = false
   isShipMovingDown = false
-}
 
-export let airfighter = new Player();
+  render() {
+    const afterForardDesccelerationCondition =
+      this.vx > 0 && this.isShipMovingRight == false
+    const afterBackDesccelerationCondition =
+      this.vx < 0 && this.isShipMovingLeft == false
 
-export function moveAirfighterToInitalPosition() {
-  airfighter.x = 0;
-  airfighter.y = 0;
+    this.element.style.left = this.x
+    this.element.style.top = this.y
+    this.health.element.style.left = this.x
+    this.health.element.style.top = this.y
+    this.health.element.style.width = this.width
+    this.health.element.style.height = this.height * 0.1
+    this.healthtext.element.value =
+      this.health.element.value / this.health.element.max
+    this.healthtext.element.innerHTML = `${this.health.element.value} / ${this.health.element.max} HP`
+    this.healthtext.element.style.left = this.x
+    this.healthtext.element.style.top = this.y - 35
+    this.healthtext.element.style.width = this.width
 
-  rocket.x = airfighter.rocketDefaultX;
-  rocket.y = airfighter.rocketDefaultY;
-}
-
-export function renderShip() {
-  const afterForardDesccelerationCondition = airfighter.vx > 0 && airfighter.isShipMovingRight == false;
-  const afterBackDesccelerationCondition = airfighter.vx < 0 && airfighter.isShipMovingLeft == false;
-  airfighter.element.style.left = airfighter.x;
-  airfighter.element.style.top = airfighter.y;
-  airfighter.health.element.style.left = airfighter.x;
-  airfighter.health.element.style.top = airfighter.y;
-  airfighter.health.element.style.width = airfighter.width;
-  airfighter.health.element.style.height = airfighter.height*0.1;
-  airfighter.healthtext.element.value = airfighter.health.element.value / airfighter.health.element.max;
-  airfighter.healthtext.element.innerHTML = `${airfighter.health.element.value} / ${airfighter.health.element.max} HP`;
-  airfighter.healthtext.element.style.left = airfighter.x;
-  airfighter.healthtext.element.style.top = airfighter.y - 35;
-  airfighter.healthtext.element.style.width = airfighter.width;
-  
-  if (afterForardDesccelerationCondition) {
-    afterForardDescceleration();
-    if (rocket.velocity < 7) {
-      rocket.x += airfighter.vx;
+    if (afterForardDesccelerationCondition) {
+      afterForardDescceleration()
+      if (rocket.velocity < 7) {
+        rocket.x += this.vx
+      }
     }
-  }  
 
-  if (afterBackDesccelerationCondition) {
-    afterBackDescceleration();
-    if (rocket.velocity < 7) {
-      rocket.x += airfighter.vx;
+    if (afterBackDesccelerationCondition) {
+      afterBackDescceleration()
+      if (rocket.velocity < 7) {
+        rocket.x += this.vx
+      }
     }
+  }
+
+  moveToInitalPosition() {
+    this.x = 0
+    this.y = 0
   }
 }
 
+export let airfighter = new Player()
 
 function accelerationBack() {
-  const maxBackSpeed = airfighter.vx < -5;
+  const maxBackSpeed = airfighter.vx < -5
   if (airfighter.x > 0) {
-    airfighter.x += airfighter.vx;
+    airfighter.x += airfighter.vx
     if (!maxBackSpeed) {
-      airfighter.vx -= acceleration / 2;
+      airfighter.vx -= acceleration / 2
     }
   }
 }
 
 function accelerationForward() {
-  const maxForwardSpeed = airfighter.vx >= 10;
+  const maxForwardSpeed = airfighter.vx >= 10
   if (airfighter.x + airfighter.width < screen.width) {
-    airfighter.x += airfighter.vx;
+    airfighter.x += airfighter.vx
     if (!maxForwardSpeed) {
-      airfighter.vx += acceleration;
+      airfighter.vx += acceleration
     }
   }
 }
 
 function afterForardDescceleration() {
-  airfighter.x += airfighter.vx;
-  airfighter.vx -= acceleration / 4;
+  airfighter.x += airfighter.vx
+  airfighter.vx -= acceleration / 4
 }
 
 function afterBackDescceleration() {
-  airfighter.x += airfighter.vx;
-  airfighter.vx += acceleration / 8;
+  airfighter.x += airfighter.vx
+  airfighter.vx += acceleration / 8
 }
-
 
 export function moveShipDown() {
-  if (airfighter.y + airfighter.height > window.innerHeight-50 ) {
-    document.querySelector('#gameover-screen').style.display = '';
-    airfighter.x = 0;
-    airfighter.y = 0;
-    soundRocketHit.pause();
-    soundEnemyDieExplosion.play();
-    setTimeout (function() {
-      soundMainTheme.pause();
-      soundGameOver.play();
-    }, 900);
+  if (airfighter.y + airfighter.height > window.innerHeight - 50) {
+    document.querySelector('#gameover-screen').style.display = ''
+    airfighter.x = 0
+    airfighter.y = 0
+    soundRocketHit.pause()
+    soundEnemyDieExplosion.play()
+    setTimeout(function () {
+      soundMainTheme.pause()
+      soundGameOver.play()
+    }, 900)
   }
-  airfighter.y += 10;
+  airfighter.y += 10
   if (rocket.velocity < 7) {
-    rocket.y += 10;
-  } 
+    rocket.y += 10
+  }
 }
-
 
 export function moveShipLeft() {
   if (airfighter.x > 0) {
-    accelerationBack();
+    accelerationBack()
     if (rocket.velocity < 7) {
-      rocket.x += airfighter.vx;
+      rocket.x += airfighter.vx
     }
   }
 }
-  
-export function moveShipRight() {
-  accelerationForward();
-  if (rocket.velocity < 7) {
-    rocket.x += airfighter.vx;
-  }
 
+export function moveShipRight() {
+  accelerationForward()
+  if (rocket.velocity < 7) {
+    rocket.x += airfighter.vx
+  }
 }
-  
+
 export function moveShipUp() {
   if (airfighter.y > 0) {
-    airfighter.y -= 10;
+    airfighter.y -= 10
     if (rocket.velocity < 7) {
-      rocket.y -= 10;
+      rocket.y -= 10
     }
   }
 }
