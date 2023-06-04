b9b1ad5021c29e615cbeef4ea9393502e95937ff
19thanks96 <19thanks96@users.noreply.github.com>
2023-06-04 12:32:23 +0000

    Remove preview for PR 132 🛬

--- pr-preview/pr-132/src/ammo.js
+++
@@ -1,7 +0,0 @@
-import { gatling } from "./gatling.js";
-import { rocket } from "./rocket.js"; 
-
-export function resetAmmo() {
-    gatling.ammo = 1500
-    rocket.ammo = 10
-}
--- pr-preview/pr-132/src/rendering/Helpers.js
+++
@@ -1,3 +0,0 @@
-export const deadEnemyXPosition = -9999
-export const rocketDefaultX = 80
-export const rocketDefaultY = 70
