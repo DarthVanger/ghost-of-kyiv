d45564aba985e25b7bfaf0592cbfec3789190e1c
DarthVanger <darthvanger@gmail.com>
2023-06-09 18:50:21 +0300

    Create keyboard.js and gameMenuActions.js files

--- src/gameManager.js
+++ src/gameManager.js
@@ -1,4 +1,3 @@
-import { mobileControls } from './touch.js'
 import { rocket } from './rocket.js'
 import { airfighter } from './airfighter.js'
 import { deleteEnemies, createEnemies, enemies } from './enemy.js'
@@ -10,11 +9,11 @@ import {
   soundboss,
 } from './music.js'
 import { Step, gameState, fps } from './step.js'
-import { fireGatlingEnemy } from './gatling.js'
 import { resetAmmo } from './ammo.js'
 import { level3Boss, createBoss } from './Boss.js'
 import { bossPopup } from './rendering/Helpers.js'
 import { resetBackground } from './background.js'
+import { initKeybordMovement } from './keyboard.js'
 
 let isGameStarted = false
 let introduction = document.querySelector('#introduction')
@@ -122,135 +121,6 @@ function startLevel3() {
 
 document.querySelector('#nextlevel').addEventListener('click', changeLevel)
 
-function initKeybordMovement() {
-  document.addEventListener('keydown', handleKeyDown)
-  document.addEventListener('keyup', handleKeyUp)
-  mobileControls.leftButton.addEventListener('touchstart', function () {
-    airfighter.isShipMovingLeft = true
-  })
-  mobileControls.rightButton.addEventListener('touchstart', function () {
-    airfighter.isShipMovingRight = true
-  })
-  mobileControls.topButton.addEventListener('touchstart', function () {
-    airfighter.isShipMovingUp = true
-  })
-  mobileControls.bottomButton.addEventListener('touchstart', function () {
-    airfighter.isShipMovingDown = true
-  })
-  mobileControls.leftButton.addEventListener('touchend', function () {
-    airfighter.isShipMovingLeft = false
-  })
-  mobileControls.rightButton.addEventListener('touchend', function () {
-    airfighter.isShipMovingRight = false
-  })
-  mobileControls.topButton.addEventListener('touchend', function () {
-    airfighter.isShipMovingUp = false
-  })
-  mobileControls.bottomButton.addEventListener('touchend', function () {
-    airfighter.isShipMovingDown = false
-  })
-  mobileControls.fireButton.addEventListener('click', fireRocket)
-}
-
-function handleKeyDown(event) {
-  if ((event.key == 'r' || event.key == 'к') && rocket.ammo != 0) {
-    fireRocket()
-  }
-
-  if (event.key == 'a' || event.key == 'ф') {
-    airfighter.isShipMovingLeft = true
-    if (airfighter.element.src != 'img/aifighter-Back.gif') {
-      airfighter.element.src = 'img/aifighter-Back.gif'
-    }
-  }
-
-  if (event.key == 's' || event.key == 'ы' || event.key == 'і') {
-    airfighter.isShipMovingDown = true
-    if (airfighter.element.src != 'img/aifighter-Down.gif') {
-      airfighter.element.src = 'img/aifighter-Down.gif'
-    }
-  }
-
-  if (event.key == 'w' || event.key == 'ц') {
-    airfighter.isShipMovingUp = true
-    if (airfighter.element.src != 'img/aifighter-Up.gif') {
-      airfighter.element.src = 'img/aifighter-Up.gif'
-    }
-  }
-
-  if (event.key == 'd' || event.key == 'в') {
-    airfighter.isShipMovingRight = true
-    if (airfighter.element.src != 'img/aifighter-Front-Accelerate.gif') {
-      airfighter.element.src = 'img/aifighter-Front-Accelerate.gif'
-    }
-  }
-
-  if (event.key == 'p' || event.key == 'з') {
-    gamePauseAction()
-  }
-  if (event.key == ' ') {
-    fireGatlingEnemy()
-  }
-}
-
-function handleKeyUp(event) {
-  if (event.key == 'a' || event.key == 'ф') {
-    airfighter.isShipMovingLeft = false
-    airfighter.element.src = 'img/aifighter-Front.gif'
-  }
-
-  if (event.key == 's' || event.key == 'ы' || event.key == 'і') {
-    airfighter.isShipMovingDown = false
-    airfighter.element.src = 'img/aifighter-Front.gif'
-  }
-
-  if (event.key == 'w' || event.key == 'ц') {
-    airfighter.isShipMovingUp = false
-    airfighter.element.src = 'img/aifighter-Front.gif'
-  }
-
-  if (event.key == 'd' || event.key == 'в') {
-    airfighter.isShipMovingRight = false
-    airfighter.element.src = 'img/aifighter-Front.gif'
-  }
-}
-
-function fireRocket() {
-  if (rocket.velocity < 8) {
-    rocket.velocity += 8
-    rocket.ammo -= 1
-    rocket.element.src = 'img/Rocket.gif'
-    setTimeout(preRocket, 8)
-    soundRocketShot.play()
-  }
-}
-
-function preRocket() {
-  rocket.element.src = 'img/mrRocket.gif'
-}
-
 export function endGameAction() {
   document.location.reload()
-}
-
-function gamePauseAction() {
-  if (gameState.isGamePaused) {
-    unPauseGame()
-  } else {
-    pauseGame()
-  }
-}
-
-const pauseScreen = document.querySelector('#pause-screen')
-
-function unPauseGame() {
-  gameState.gameIntervalId = setInterval(Step, 1000 / fps)
-  pauseScreen.style.display = 'none'
-  gameState.isGamePaused = false
-}
-
-function pauseGame() {
-  clearInterval(gameState.gameIntervalId)
-  pauseScreen.style.display = 'block'
-  gameState.isGamePaused = true
 }
