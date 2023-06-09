24e7e3142ca164ea3ca1dc92dd18a93df0a8cb78
DarthVanger <darthvanger@gmail.com>
2023-06-10 00:24:36 +0300

    Use functions instead of class for PlayerMovement

--- src/player/PlayerMovement.js
+++ src/player/PlayerMovement.js
@@ -7,86 +7,78 @@ import {
 } from '../music.js'
 import { rocket } from '../rocket.js'
 
-class PlayerMovement {
-  acceleration = 1
+const acceleration = 1
 
-  constructor(player) {
-    this.player = player
-  }
-
-  accelerationBack() {
-    const maxBackSpeed = this.player.vx < -5
-    if (this.player.x > 0) {
-      this.player.x += this.player.vx
-      if (!maxBackSpeed) {
-        this.player.vx -= acceleration / 2
-      }
-    }
-  }
-
-  accelerationForward() {
-    const maxForwardSpeed = this.player.vx >= 10
-    if (this.player.x + this.player.width < screen.width) {
-      this.player.x += this.player.vx
-      if (!maxForwardSpeed) {
-        this.player.vx += acceleration
-      }
-    }
-  }
-
-  afterForardDescceleration() {
-    this.player.x += this.player.vx
-    this.player.vx -= acceleration / 4
-  }
-
-  afterBackDescceleration() {
-    this.player.x += this.player.vx
-    this.player.vx += acceleration / 8
-  }
-
-  moveShipDown() {
-    if (this.player.y + this.player.height > window.innerHeight - 50) {
-      document.querySelector('#gameover-screen').style.display = ''
-      this.player.x = 0
-      this.player.y = 0
-      soundRocketHit.pause()
-      soundEnemyDieExplosion.play()
-      setTimeout(function () {
-        soundMainTheme.pause()
-        soundboss.pause()
-        soundGameOver.play()
-      }, 900)
-    }
-    this.player.y += 10
-    if (rocket.velocity < 7) {
-      rocket.y += 10
-    }
-  }
-
-  moveShipLeft() {
-    if (this.player.x > 0) {
-      accelerationBack(this.player)
-      if (rocket.velocity < 7) {
-        rocket.x += this.player.vx
-      }
-    }
-  }
-
-  moveShipRight() {
-    accelerationForward(this.player)
-    if (rocket.velocity < 7) {
-      rocket.x += this.player.vx
-    }
-  }
-
-  moveShipUp() {
-    if (this.player.y > 0) {
-      this.player.y -= 10
-      if (rocket.velocity < 7) {
-        rocket.y -= 10
-      }
+function accelerationBack(player) {
+  const maxBackSpeed = player.vx < -5
+  if (player.x > 0) {
+    player.x += player.vx
+    if (!maxBackSpeed) {
+      player.vx -= acceleration / 2
     }
   }
 }
 
-export default PlayerMovement
+function accelerationForward(player) {
+  const maxForwardSpeed = player.vx >= 10
+  if (player.x + player.width < screen.width) {
+    player.x += player.vx
+    if (!maxForwardSpeed) {
+      player.vx += acceleration
+    }
+  }
+}
+
+export function afterForardDescceleration(player) {
+  player.x += player.vx
+  player.vx -= acceleration / 4
+}
+
+export function afterBackDescceleration(player) {
+  player.x += player.vx
+  player.vx += acceleration / 8
+}
+
+export function moveShipDown(player) {
+  if (player.y + player.height > window.innerHeight - 50) {
+    document.querySelector('#gameover-screen').style.display = ''
+    player.x = 0
+    player.y = 0
+    soundRocketHit.pause()
+    soundEnemyDieExplosion.play()
+    setTimeout(function () {
+      soundMainTheme.pause()
+      soundboss.pause()
+      soundGameOver.play()
+    }, 900)
+  }
+  player.y += 10
+  if (rocket.velocity < 7) {
+    rocket.y += 10
+  }
+}
+
+export function moveShipLeft(player) {
+  if (player.x > 0) {
+    accelerationBack(player)
+    if (rocket.velocity < 7) {
+      rocket.x += player.vx
+    }
+  }
+}
+
+export function moveShipRight(player) {
+  accelerationForward(player)
+  if (rocket.velocity < 7) {
+    rocket.x += player.vx
+  }
+}
+
+export function moveShipUp(player) {
+  if (player.y > 0) {
+    player.y -= 10
+    if (rocket.velocity < 7) {
+      rocket.y -= 10
+    }
+  }
+}
