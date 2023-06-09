bd424433965d2d2b545f02dc3c9fcacf35542571
DarthVanger <DarthVanger@users.noreply.github.com>
2023-06-09 19:07:34 +0000

    Remove preview for PR 140 🛬

--- pr-preview/pr-140/src/levelCompleteScreen.js
+++
@@ -1,8 +0,0 @@
-export const levelCompleteScreen = document.createElement('div')
-
-levelCompleteScreen.innerHTML = `
-  <h2>Level Complete!</h2>
-  <br /><br /><button id="nextLevel" type="button">Next level</button><br />
-  <img src="img/level_complete_animation.gif" />
-`
-levelCompleteScreen.id = "levelComplete"
