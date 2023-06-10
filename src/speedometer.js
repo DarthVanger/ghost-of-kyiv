ed8a74aa483dc85d14b441fd0f5d36514cd45a4a
DarthVanger <darthvanger@gmail.com>
2023-06-10 21:32:52 +0000

    Deploying to gh-pages from @ DarthVanger/ghost-of-kyiv@cd83ffd9d93b973822901ba3e992e3b9103c68be 🚀

--- src/speedometer.js
+++ src/speedometer.js
@@ -4,13 +4,18 @@ speedometer.id = 'speedometer'
 const vxElement = document.createElement('div')
 vxElement.innerHTML = 'VX: 0'
 
+const vyElement = document.createElement('div')
+vyElement.innerHTML = 'VY: 0'
+
 const axElement = document.createElement('div')
 axElement.innerHTML = 'AX: 0'
 
 speedometer.append(vxElement)
+speedometer.append(vyElement)
 speedometer.append(axElement)
 
-export function updateSpeedometer(vx, ax) {
+export function updateSpeedometer(vx, ax, vy) {
   vxElement.innerHTML = `VX: ${vx}`
   axElement.innerHTML = `AX: ${ax}`
+  vyElement.innerHTML = `VY: ${vy}`
 }
