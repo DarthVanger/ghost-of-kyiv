9a7932366e7a677e2c66e10a6f385eeeac5f8fae
19thanks96 <19thanks96@users.noreply.github.com>
2023-06-13 20:36:05 +0000

    Deploy preview for PR 157 🛫

--- pr-preview/pr-157/src/levelCompleteScreen.js
+++ pr-preview/pr-157/src/levelCompleteScreen.js
@@ -10,6 +10,6 @@ levelCompleteScreen.innerHTML = levelCompleteScreenText
 levelCompleteScreen.id = 'levelComplete'
 
 
-export let bossCompleteScreen = `<br /><h2>Boss Defeated</h2>
+export let bossCompleteScreenText = `<br /><h2>Boss Defeated</h2>
 <br /><br /><button id="nextLevel" type="button">Next level</button><br />
 <img src="img/bossDefeated.gif" />`
