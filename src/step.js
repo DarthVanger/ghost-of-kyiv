440f965316df6dfc4102ee89255f758601c9dd4e
Allias <oleksandr.lysenko.work@gmail.com>
2023-06-13 18:54:44 +0200

    Refactored level over

--- src/step.js
+++ src/step.js
@@ -2,9 +2,8 @@ import { enemies, updateEnemy } from './enemy.js'
 import { rocket, renderRocket, moveRocket } from './rocket.js'
 import { bulletArray, gatling, moveBullet } from './gatling.js'
 import { airfighter } from './player/Player.js'
-import { soundMainTheme, soundLevelComplete } from './music.js'
 import { moveBackground } from './background.js'
-import { levelCompleteScreen } from './levelCompleteScreen.js'
+import { levelOverIfLastEnemyOut } from './gameOver.js'
 
 export const fps = 60
 export const gameState = {
@@ -23,26 +22,9 @@ export function Step() {
   airfighter.render()
 
   const lastEnemy = enemies[enemies.length - 1]
-  const vuletivZaRamku = lastEnemy.x < 0 - lastEnemy.width
-  if (vuletivZaRamku) {
-    stopInterval()
-    document.body.append(levelCompleteScreen)
-    fadeIn(levelCompleteScreen)
-    soundMainTheme.pause()
-    soundMainTheme.currentTime = 0
-    soundLevelComplete.play()
-  }
+  levelOverIfLastEnemyOut(lastEnemy, gameState)
 }
 
 function changeAmmo() {
   ammoElement.innerHTML = `<img class="ammoImg" src="img/ammo-gatling-img.gif"> ${gatling.ammo} <br> <img class="ammoImg" src="img/ammo-rocket-img.gif"> ${rocket.ammo}`
-}
-
-function fadeIn(element) {
-  element.className = 'fadeIn'
-}
-
-export function stopInterval() {
-  clearInterval(gameState.gameIntervalId)
-  gameState.isGamePaused = true
 }
