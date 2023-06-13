b15794823ebd674b6db814d77a418b70c493f58c
callisto888 <callisto888@users.noreply.github.com>
2023-06-13 18:01:46 +0000

    Deploy preview for PR 156 🛫

---
+++ pr-preview/pr-156/src/Boss.js
@@ -0,0 +1,95 @@
+import { addGatling } from './gatling.js'
+import { createRocket } from './enemy.js'
+export let level3Boss
+
+function createBossHp(boss, i, maxHealth) {
+  boss.enemyHealth = {}
+  boss.enemyHealth.velocity = -2
+  boss.enemyHealth.element = document.createElement('meter')
+  boss.enemyHealth.element.setAttribute('min', 0)
+  boss.enemyHealth.element.setAttribute('low', maxHealth / 3)
+  boss.enemyHealth.element.setAttribute('high', (maxHealth / 3) * 2)
+  boss.enemyHealth.element.setAttribute('optimum', maxHealth)
+  boss.enemyHealth.element.setAttribute('max', maxHealth)
+  boss.enemyHealth.element.setAttribute('value', maxHealth)
+  boss.enemyHealth.element.id = 'healthBar200-' + i
+  boss.enemyHealth.element.className = 'healthBar200'
+  document.body.append(boss.enemyHealth.element)
+
+  boss.enemyHealthText = {}
+  boss.enemyHealthText.velocity = -2
+  boss.enemyHealthText.element = document.createElement('div')
+  boss.enemyHealthText.element.id = 'healthBar200text-' + i
+  boss.enemyHealthText.element.className = 'healthBar200text'
+  document.body.append(boss.enemyHealthText.element)
+}
+
+function createBossElement(src, width, height, i, maxHealth) {
+  let boss = {}
+  boss.element = document.createElement('img')
+  boss.element.id = 'boss'
+  boss.element.className = 'boss'
+  boss.element.src = src
+  boss.x = Math.floor(Math.random() * 400) + window.innerWidth
+  boss.y = Math.floor(Math.random() * (innerHeight - 300) + 50)
+  boss.width = width
+  boss.height = height
+  boss.velocity = -2
+  boss.velocityY = 2
+  boss.isAlive = true
+  createBossHp(boss, i, maxHealth)
+  return boss
+}
+
+export function createBoss() {
+  let boss
+  boss = createBossElement('img/boss.webp', 250, 80, 1, 1000)
+
+  const bossRocketImg = document.createElement('img')
+  bossRocketImg.className = 'enemyRocket'
+  bossRocketImg.src = 'img/mrRocket.gif'
+  boss.isRocketLaunched = false
+  document.body.append(bossRocketImg)
+
+  boss.rocket = {
+    x: boss.x,
+    y: boss.y,
+    width: 120,
+    height: 12,
+    dmg: 200,
+    vx: 0,
+    vy: 0,
+    element: bossRocketImg,
+  }
+  boss.behavior = bossbehavior
+  addGatling(boss)
+  document.body.append(boss.element)
+  level3Boss = boss
+}
+
+function bossbehavior() {
+  const bossAppearance = this.x < window.innerWidth - this.width * 1.1
+  if (bossAppearance) {
+    this.velocity = 0
+
+    const topMotionFrame = this.y < 0 + this.height / 2
+    if (topMotionFrame) {
+      this.velocityY *= -1
+    }
+    const bottobMotionFrame = this.y > window.innerHeight - this.height * 2
+    if (bottobMotionFrame) {
+      this.velocityY *= -1
+    }
+    this.y -= this.velocityY
+
+    const restartRocket = this.rocket.x < 0 - this.rocket.width * 2
+    if (restartRocket) {
+      this.rocket.x = this.x
+      this.rocket.y = this.y
+    }
+    if (this.rocket.vx == 0) {
+      const ammoDamage = Math.floor(this.enemyHealth.element.value / 10)
+      createRocket(this, ammoDamage)
+    }
+  }
+}
---
+++ pr-preview/pr-156/src/background.js
@@ -0,0 +1,36 @@
+let backBgX = 0
+let middleBgX = 0
+let frontBgX = 0
+let backBgVx = -0.1
+let middleBgVx = -0.3
+let frontBgDefaultVx = -5
+let frontBg = document.querySelector('#bg-front')
+
+export function moveBackground(airfighter) {
+  let frontBgVx = frontBgDefaultVx - airfighter.vx / 4
+  let backBg = document.querySelector('#bg-back')
+  let middleBg = document.querySelector('#bg-middle')
+  if (backBgX < window.innerWidth - backBg.width) {
+    backBgVx *= -1
+  }
+  if (middleBgX < window.innerWidth - middleBg.width) {
+    middleBgVx *= -1
+  }
+  if (backBgX > 0) {
+    backBgVx *= -1
+  }
+  if (middleBgX > 0) {
+    middleBgVx *= -1
+  }
+  backBgX += backBgVx
+  middleBgX += middleBgVx
+  frontBgX += frontBgVx
+  backBg.style.left = backBgX + 'px'
+  middleBg.style.left = middleBgX + 'px'
+  frontBg.style.left = frontBgX + 'px'
+}
+
+export function resetBackground() {
+  frontBgX = 0
+  frontBg.style.left = frontBgX + 'px'
+}
