fcae388d93b7b691a491c939ca4b63402a6f854b
callisto888 <a11ias@hotmail.com>
2023-06-13 17:48:05 +0000

    Deploying to gh-pages from @ DarthVanger/ghost-of-kyiv@2f51567075e3cec40484bf5220b2f9edd61e3cc7 🚀

--- src/gameOver.js
+++ src/gameOver.js
@@ -4,7 +4,9 @@ import {
   soundMainTheme,
   soundGameOver,
   soundboss,
+  soundLevelComplete
 } from './music.js'
+import { levelCompleteScreen } from './levelCompleteScreen.js'
 
 export function gameOver() {
   document.querySelector('#gameover-screen').style.display = ''
@@ -15,4 +17,25 @@ export function gameOver() {
     soundboss.pause()
     soundGameOver.play()
   }, 900)
+}
+
+export function levelOverIfLastEnemyOut(lastEnemy, gameState) {
+  const vuletivZaRamku = lastEnemy.x < 0 - lastEnemy.width
+  if (vuletivZaRamku) {
+    stopInterval(gameState)
+    document.body.append(levelCompleteScreen)
+    fadeIn(levelCompleteScreen)
+    soundMainTheme.pause()
+    soundMainTheme.currentTime = 0
+    soundLevelComplete.play()
+  }
+}
+
+function fadeIn(element) {
+  element.className = 'fadeIn'
+}
+
+function stopInterval(gameState) {
+  clearInterval(gameState.gameIntervalId)
+  gameState.isGamePaused = true
 }
