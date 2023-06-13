2affe9bf2fd71950edc8b9ddcb5d3572d0c0321f
DarthVanger <darthvanger@gmail.com>
2023-06-14 00:34:39 +0300

    Ammo: update only text, not images
    Render images on website load.
    In step() render only ammo text.

--- src/ammo.js
+++ src/ammo.js
@@ -8,16 +8,34 @@ export function resetAmmo(bullets, rockets) {
 
 const ammoElement = document.createElement('div')
 
+const ammoImgContainer = document.createElement('div')
+const ammoCountElement = document.createElement('div')
+ammoCountElement.id = 'ammoCountElement'
+
 ammoElement.id = 'ammoElement'
+
+ammoElement.append(ammoImgContainer)
+ammoElement.append(ammoCountElement)
+
+addAmmoImages()
 
 document.body.append(ammoElement)
 
 export function renderAmmo() {
-  ammoElement.innerHTML = `
+  renderAmmoCount()
+}
+
+function addAmmoImages() {
+  ammoImgContainer.innerHTML = `
     <img class="ammoImg" src="img/ammo-gatling-img.gif">
-    ${gatling.ammo}
     <br>
     <img class="ammoImg" src="img/ammo-rocket-img.gif">
-    ${rocket.ammo}
+  `
+}
+
+function renderAmmoCount() {
+  ammoCountElement.innerHTML = `
+    <div id="gatling-ammo">${gatling.ammo}</div>
+    <div id="rocket-ammo">${rocket.ammo}</div>
   `
 }
