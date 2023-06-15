571ace6884c1f84bf153424a1851a93ed85b2d6e
Alianshark <Alianshark@users.noreply.github.com>
2023-06-15 11:05:07 +0000

    Deploy preview for PR 163 🛫

---
+++ pr-preview/pr-163/src/step.js
@@ -0,0 +1,27 @@
+import { enemies, updateEnemy } from './enemy.js'
+import { rocket, renderRocket, moveRocket } from './rocket.js'
+import { bulletArray, gatling, moveBullet } from './gatling.js'
+import { airfighter } from './player/Player.js'
+import { moveBackground } from './background.js'
+import { levelOverIfLastEnemyOut } from './gameOver.js'
+import { renderAmmo } from './ammo.js'
+
+export const fps = 60
+export const gameState = {
+  isGamePaused: false,
+  gameIntervalId: undefined,
+}
+
+export function Step() {
+  enemies.forEach(updateEnemy)
+  bulletArray.forEach(moveBullet)
+  moveRocket()
+  renderAmmo()
+  moveBackground(airfighter)
+
+  renderRocket()
+  airfighter.render()
+
+  const lastEnemy = enemies[enemies.length - 1]
+  levelOverIfLastEnemyOut(lastEnemy, gameState)
+}
