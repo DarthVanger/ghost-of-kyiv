5d3f53dbb3037030315b4ae72488b5d1bddfaad0
Gehinom <Gehinom@users.noreply.github.com>
2023-06-23 21:55:25 +0000

    Deploy preview for PR 172 🛫

--- pr-preview/pr-172/electron-app.js
+++ pr-preview/pr-172/electron-app.js
@@ -2,9 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    show: false,
   })
+  win.maximize()
+  win.show()
 
   win.loadFile('index.html')
 }
