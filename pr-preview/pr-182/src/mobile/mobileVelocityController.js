16e52d26f40c96c06cfdc392646cb833b694c104
19thanks96 <19thanks96@users.noreply.github.com>
2023-10-07 17:33:09 +0000

    Deploy preview for PR 182 🛫

--- pr-preview/pr-182/src/mobile/mobileVelocityController.js
+++ pr-preview/pr-182/src/mobile/mobileVelocityController.js
@@ -1,24 +1,25 @@
+
 export function setAirfighterVelocityFromMobileJoystick(
   airfighter,
-  isMobileDevice
+  mobileJoystick
 ) {
-  if (isMobileDevice.x < 0) {
+  if (mobileJoystick.x < 0) {
     airfighter.isKeyLeftPressed = true
-    airfighter.vx *= isMobileDevice.x * -1
-  } else if (isMobileDevice.x > 0) {
+    airfighter.vx *= mobileJoystick.x * -1
+  } else if (mobileJoystick.x > 0) {
     airfighter.isKeyRightPressed = true
-    airfighter.vx *= isMobileDevice.x
+    airfighter.vx *= mobileJoystick.x
   } else {
     airfighter.isKeyRightPressed = false
     airfighter.isKeyLeftPressed = false
   }
 
-  if (isMobileDevice.y < 0) {
+  if (mobileJoystick.y < 0) {
     airfighter.isKeyUpPressed = true
-    airfighter.vy *= isMobileDevice.y * -1
-  } else if (isMobileDevice.y > 0) {
+    airfighter.vy *= mobileJoystick.y * -1
+  } else if (mobileJoystick.y > 0) {
     airfighter.isKeyDownPressed = true
-    airfighter.vy *= isMobileDevice.y
+    airfighter.vy *= mobileJoystick.y
   } else {
     airfighter.isKeyUpPressed = false
     airfighter.isKeyDownPressed = false
---
+++ pr-preview/pr-182/src/mobile/preventDefault.js
@@ -0,0 +1,9 @@
+document.addEventListener('touchstart', (event) => {
+    event.preventDefault()
+})
+document.addEventListener('touchmove', (event) => {
+    event.preventDefault()
+})
+document.addEventListener('touchend', (event) => {
+    event.preventDefault()
+})
