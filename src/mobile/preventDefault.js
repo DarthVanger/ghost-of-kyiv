f1eb36a7bef02672aa502ee4e4abc151484f08a5
hellgamer <19thanks96@gmail.com>
2023-10-08 12:44:17 +0300

    delete notworking code

--- src/mobile/preventDefault.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('touchstart', (event) => {
-    event.preventDefault()
-})
-document.addEventListener('touchmove', (event) => {
-    event.preventDefault()
-})
-document.addEventListener('touchend', (event) => {
-    event.preventDefault()
-})
