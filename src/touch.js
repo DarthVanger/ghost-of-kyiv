33c3e51b794a3d09a9ae64657b6cef1ee2cf96b3
hellgamer <19thanks96@gmail.com>
2023-10-10 17:53:40 +0300

    patched mobcontrols and add info for him

--- src/touch.js
+++ src/touch.js
@@ -1,6 +1,5 @@
-import {createJoystick} from './mobileJoystick.js'
+import { createJoystick } from './mobileJoystick.js'
 export const mobileControls = {
-  
   fireButton: document.querySelector('.fire'),
 }
 
@@ -15,7 +14,7 @@ if (
   navigator.userAgent.match('RIM')
 ) {
   document.body.classList.add('_touch')
-  touch.style.display = 'none'
+  touch.innerHTML = `<br><br><h2>Use the bottom left joystick to move around.<h2><br><br>Use the gray in the lower right corner to launch a rocket.<br><br><br>Click on an enemy to launch a bullet.`
   createJoystick()
   if (window.orientation === 0) {
     viewport.setAttribute(
