538af843ec3e792e69bc805d894336cf7dfcf8c7
19thanks96 <19thanks96@users.noreply.github.com>
2023-10-11 09:23:39 +0000

    Remove preview for PR 183 🛬

--- pr-preview/pr-183/src/rendering/Explosion.js
+++
@@ -1,6 +0,0 @@
-export let explosion = document.createElement('img')
-explosion.id = 'explosion-' + new Date()
-explosion.className = 'explosion'
-explosion.src = './img/explosion.gif'
-explosion.width = 120
-explosion.height = 130
