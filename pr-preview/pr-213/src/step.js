26ecdf01e8db35d9b4e63ad4ac7cd86653aebecc
DarthVanger <DarthVanger@users.noreply.github.com>
2023-12-03 17:12:29 +0000

    Deploy preview for PR 213 🛫

---
+++ pr-preview/pr-213/src/step.js
@@ -0,0 +1,33 @@
+import { enemies, updateEnemy } from './enemy.js'
+import { renderRocket, moveRocket } from './rocket.js'
+import { bulletArray, moveBullet } from './gatling.js'
+import { moveBackground } from './background.js'
+import { levelOverIfLastEnemyOut } from './gameOver.js'
+import { renderAmmo } from './ammo.js'
+import { mobileJoystick } from './mobileJoystick.js'
+import { setAirfighterVelocityFromMobileJoystick } from './mobile/mobileVelocityController.js'
+import { gameState } from './gameState.js'
+export const fps = 60
+
+export function Step() {
+  const airfighter = gameState.airfighter
+
+  enemies.forEach(updateEnemy)
+  bulletArray.forEach(moveBullet)
+  moveRocket()
+  renderAmmo()
+  moveBackground(airfighter)
+
+  renderRocket()
+  airfighter.render()
+
+  if (mobileJoystick.isMobileDevice) {
+    setAirfighterVelocityFromMobileJoystick(airfighter, mobileJoystick)
+  }
+
+  const lastEnemy = enemies[enemies.length - 1]
+  levelOverIfLastEnemyOut(lastEnemy, gameState)
+  if (!gameState.isGamePaused) {
+    requestAnimationFrame(Step)
+  }
+}
