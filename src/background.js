8e6cc38bbba506ecab70dd1556c222a702c2d3f2
hellgamer <19thanks96@gmail.com>
2023-12-20 16:29:24 +0200

    fix backbg movement

--- src/background.js
+++ src/background.js
@@ -4,7 +4,7 @@ let frontBgX = 0
 let frontBgY = 0
 let middleBgY = 0
 let backBgY = 0
-let backBgVx = -0.1
+let backBgVx = 0.1
 let middleBgVx = -0.3
 let frontBgDefaultVx = -5
 let frontBg = document.querySelector('#bg-front')
@@ -12,8 +12,10 @@ let frontBg = document.querySelector('#bg-front')
 export function moveBackground(airfighter) {
   let frontBgVx = frontBgDefaultVx - airfighter.vx / 4
   let backBg = document.querySelector('#bg-back')
+  
   let middleBg = document.querySelector('#bg-middle')
-  if (backBgX < window.innerWidth - backBg.width) {
+  console.log(backBg.getBoundingClientRect().width,backBgX < window.innerWidth - backBg.getBoundingClientRect().width)
+  if (backBgX < window.innerWidth - backBg.getBoundingClientRect().width) {
     backBgVx *= -1
   }
   if (middleBgX < window.innerWidth - middleBg.width) {
