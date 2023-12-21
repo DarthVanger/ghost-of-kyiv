44d34ce1276cdc7b404d0d9edee060b3080f17ee
DarthVanger <DarthVanger@users.noreply.github.com>
2023-12-21 19:13:17 +0000

    Remove preview for PR 228 🛬

--- pr-preview/pr-228/app.js
+++
@@ -1,5 +0,0 @@
-import { endGameAction } from './src/gameManager.js'
-import './src/startScreen.js'
-
-const endGameButton = document.querySelector('#endGameButton')
-endGameButton.addEventListener('click', endGameAction)
