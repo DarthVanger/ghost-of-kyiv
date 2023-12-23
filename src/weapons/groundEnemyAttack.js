d138ed3036560dc7eec283136e22fbec26ae8809
DarthVanger <darthvanger@gmail.com>
2023-12-23 16:51:26 +0200

    Rotate enemy ground rockets

--- src/weapons/groundEnemyAttack.js
+++ src/weapons/groundEnemyAttack.js
@@ -4,13 +4,17 @@ import { createRocket } from './enemyRocketAtack.js'
 export function createTargetedRocket(enemy) {
   const rocket = createRocket(enemy)
   const playerCenter = {
-    x: gameState.airfighter.x - gameState.airfighter.width / 2,
-    y: gameState.airfighter.y - gameState.airfighter.height / 2,
+    x: gameState.airfighter.x + gameState.airfighter.width / 2,
+    y: gameState.airfighter.y + gameState.airfighter.height / 2,
   }
   const distX = playerCenter.x - enemy.x
   const distY = playerCenter.y - enemy.y
   const dist = Math.sqrt(distX * distX + distY * distY)
-  rocket.vx = (5 * distX) / dist
-  rocket.vy = (5 * distY) / dist
+  rocket.vx = (8 * distX) / dist
+  rocket.vy = (8 * distY) / dist
+
+  rocket.rotationX = Math.atan2(distY, distX)
+  rocket.element.style.transform = `rotate(${rocket.rotationX}rad)`
+
   return rocket
 }
