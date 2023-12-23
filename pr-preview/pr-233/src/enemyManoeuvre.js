d74ca0e53f9cfe61278c69a499a34c3067c04060
DarthVanger <DarthVanger@users.noreply.github.com>
2023-12-23 14:55:16 +0000

    Deploy preview for PR 233 🛫

---
+++ pr-preview/pr-233/src/enemyManoeuvre.js
@@ -0,0 +1,40 @@
+export function manoeuvreUpAtHalfScreen(enemy) {
+  const halfScreenX = window.innerWidth / 2
+  if (enemy.x < halfScreenX) {
+    enemy.vy = -2
+  }
+}
+
+export function manoeuvreDownAtHalfScreen(enemy) {
+  const halfScreenX = window.innerWidth / 2
+  if (enemy.x < halfScreenX) {
+    enemy.vy = 2
+  }
+}
+
+export function manoeuvreZigzagAtQuarterScreen(enemy) {
+  const bottomYBound = window.innerHeight * 0.75
+  const topYBound = window.innerHeight * 0.25
+
+  if (enemy.x < window.innerWidth) {
+    if (enemy.vy === 0) {
+      enemy.vy = -1
+    }
+
+    if (enemy.y > bottomYBound) {
+      enemy.vy = -1
+    }
+
+    if (enemy.y < topYBound) {
+      enemy.vy = 1
+    }
+  }
+}
+
+export function manoeuvreStraightFast(enemy) {
+  enemy.vx = -15
+}
+export function manoeuvreOnGround(enemy) {
+  enemy.y = window.innerHeight - enemy.height + 20
+  enemy.vx = -2
+}
---
+++ pr-preview/pr-233/src/weapons/enemyRocketAtack.js
@@ -0,0 +1,60 @@
+import { createTargetedRocket } from './groundEnemyAttack.js'
+
+export function createRocket(enemy) {
+  const enemyRocketImg = document.createElement('img')
+  enemyRocketImg.className = 'enemyRocket'
+  enemyRocketImg.src = 'img/mrRocket.gif'
+  document.body.append(enemyRocketImg)
+  enemy.isRocketCooldown = false
+
+  const rocket = {
+    x: enemy.x,
+    y: enemy.y + enemy.height - enemy.height / 5,
+    width: 120,
+    height: 12,
+    dmg: enemy.rocketDmg,
+    vx: enemy.vx - 8,
+    vy: 0,
+    element: enemyRocketImg,
+  }
+  enemy.rockets.push(rocket)
+  return rocket
+}
+
+export function moveEnemyRocket(enemy) {
+  enemy.rockets.forEach((rocket) => {
+    rocket.x += rocket.vx
+    rocket.y += rocket.vy
+  })
+}
+
+export function launchRocketIfOnScreen(enemy, createRocketFunction) {
+  if (enemy.x < window.innerWidth) {
+    if (!enemy.isRocketCooldown) {
+      enemy.isRocketCooldown = true
+      setTimeout(() => {
+        enemy.isRocketCooldown = false
+        createRocketFunction(enemy)
+      }, 3000)
+    }
+  }
+}
+
+export function renderEnemyRocket(enemy) {
+  enemy.rockets.forEach((rocket) => {
+    rocket.element.style.left = rocket.x
+    rocket.element.style.top = rocket.y
+  })
+}
+
+export function updateEnemyRocketAtack(enemy) {
+  renderEnemyRocket(enemy)
+  moveEnemyRocket(enemy)
+  launchRocketIfOnScreen(enemy, createRocket)
+}
+
+export function updateEnemyGroundAtack(enemy) {
+  renderEnemyRocket(enemy)
+  moveEnemyRocket(enemy)
+  launchRocketIfOnScreen(enemy, createTargetedRocket)
+}
