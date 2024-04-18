67828a3c88df14040a19c80d1ba274addeccc912
DarthVanger <DarthVanger@users.noreply.github.com>
2024-04-18 21:20:50 +0000

    Deploy preview for PR 245 🛫

---
+++ pr-preview/pr-245/src/gameOver.js
@@ -0,0 +1,41 @@
+import {
+  soundRocketHit,
+  soundEnemyDieExplosion,
+  soundMainTheme,
+  soundGameOver,
+  soundboss,
+  soundLevelComplete,
+} from './music.js'
+import { levelCompleteScreen } from './levelCompleteScreen.js'
+
+export function gameOver() {
+  document.querySelector('#gameover-screen').style.display = ''
+  soundRocketHit.pause()
+  soundEnemyDieExplosion.play()
+  setTimeout(function () {
+    soundMainTheme.pause()
+    soundboss.pause()
+    soundGameOver.play()
+  }, 900)
+  gameState.isGamePaused = true
+}
+
+export function levelOverIfLastEnemyOut(lastEnemy, gameState) {
+  const vuletivZaRamku = lastEnemy.x < 0 - lastEnemy.width
+  if (vuletivZaRamku) {
+    stopInterval(gameState)
+    document.body.append(levelCompleteScreen)
+    fadeIn(levelCompleteScreen)
+    soundMainTheme.pause()
+    soundMainTheme.currentTime = 0
+    soundLevelComplete.play()
+  }
+}
+
+function fadeIn(element) {
+  element.className = 'fadeIn'
+}
+
+function stopInterval(gameState) {
+  gameState.isGamePaused = true
+}
