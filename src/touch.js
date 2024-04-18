68a386db759579d998844b2916877a91dbe6daa0
DarthVanger <darthvanger@gmail.com>
2024-04-19 00:15:14 +0300

    Add start screen background image
    - Add start screen background image
    - Increase controls help font-size
    - Add text-shadow for controls help

--- src/touch.js
+++ src/touch.js
@@ -5,7 +5,7 @@ export const mobileControls = {
 
 let checkDevice = navigator.userAgent
 const viewport = document.querySelector('#viewport')
-let touch = document.querySelector('#keyboard-controls-help')
+const keyboardControlsHelpElement = document.querySelector('#keyboard-controls-help')
 
 if (
   navigator.userAgent.match('iPhone') ||
@@ -14,7 +14,14 @@ if (
   navigator.userAgent.match('RIM')
 ) {
   document.body.classList.add('_touch')
-  touch.innerHTML = `<br><br><h2>Use the bottom left joystick to move around.<h2><br><br>Use the gray in the lower right corner to launch a rocket.<br><br><br>Click on an enemy to launch a bullet.`
+
+  keyboardControlsHelpElement.innerHTML = `
+    <br><br>
+    Use the bottom left joystick to move around.<br><br>
+    Use the gray in the lower right corner to launch a rocket.<br><br>
+    Click on an enemy to launch a bullet.
+  `
+
   createJoystick()
   if (window.orientation === 0) {
     viewport.setAttribute(
