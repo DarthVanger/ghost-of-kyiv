9c2f69bb9c35ad75c4f8a796f925300e6fc80176
DarthVanger <DarthVanger@users.noreply.github.com>
2024-05-04 13:53:33 +0000

    Remove preview for PR 262 🛬

--- pr-preview/pr-262/src/scores/scores.js
+++
@@ -1,9 +0,0 @@
-import './currentGameScore.js'
-
-export const scoreBonuses = {
-  killEnemyLevelOne: 20,
-  killEnemyLevelTwo: 30,
-  killEnemyLevelThree: 50,
-  killRocket: 10,
-  killBoss: 100,
-}
